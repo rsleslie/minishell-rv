e55e0784313d85adce44d6697ae8722e08a11097
Ranna Leslie Conceicao Silva <rleslie-@c1r7p3.42sp.org.br>
2023-05-10 09:49:17 -0300

    subindo signals

--- testes/tes/main.c
+++ testes/tes/main.c
@@ -1,5 +1,34 @@
 #include "../../minishell.h"
 
+void	handle_sigint(int signal, siginfo_t *info, void *context)
+{
+	(void)context;
+	(void)info;
+	
+	if (signal == SIGINT)
+	{
+		write(1, "\n", 1);
+		rl_on_new_line();
+		rl_redisplay();
+	}
+}
+
+void	init_signals(void)
+{
+	struct sigaction sigact;
+	sigset_t set;
+
+	sigact = (struct sigaction){0};
+	sigact.sa_flags = SA_SIGINFO;
+	sigact.sa_sigaction = handle_sigint;
+	sigaction(SIGINT, &sigact, NULL);
+	sigemptyset(&sigact.sa_mask);
+	sigemptyset(&set);
+    sigaddset(&set, SIGQUIT);
+    sigprocmask(SIG_BLOCK, &set, NULL);
+	sigaction(SIGQUIT, &sigact, NULL);
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	t_config    data;
@@ -9,6 +38,7 @@ int	main(int argc, char **argv, char **envp)
 
 	i = 0;
 	argc = 0;
+	init_signals();
 	data.tokens = NULL;
 	(void)argv;  
 	get_env(&env, envp);
@@ -18,12 +48,18 @@ int	main(int argc, char **argv, char **envp)
 	{
 		data.str = readline("Habla$ ");
 		add_history(data.str);
+		if (data.str == NULL)
+		{
+			write(1, "\n", 1);
+			exit(1);
+		}
 		if (*data.str && check_space(&data) != 0)
 		{
 			ft_exit(&data, env, export);
 			ft_lexer(&data);
 			parser(&data);
 		}
+
 		// i = 0;
 		// while(data.tokens[i])
 		// 	printf("%s\n", data.tokens[i++]);
